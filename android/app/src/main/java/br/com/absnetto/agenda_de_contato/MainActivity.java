b1e1e900e8a2649e0341a6f40e889a3cb95be8ca
absnetto <abs.netto@gmail.com>
2019-05-31 06:43:39 -0300

    Initial commit

---
+++ android/app/src/main/java/br/com/absnetto/agenda_de_contato/MainActivity.java
@@ -0,0 +1,13 @@
+package br.com.absnetto.agenda_de_contato;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
